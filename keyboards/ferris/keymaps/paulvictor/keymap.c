68bda265ba0d0d19438b3a6131eefb8684b0e752
Paul Victor Raj <paulvictor@gmail.com>
2023-12-13 18:06:58 +0530

    First commit

---
+++ keyboards/ferris/keymaps/paulvictor/keymap.c
@@ -0,0 +1,185 @@
+// this is the style you want to emulate.
+// This is the canonical layout file for the Quantum project. If you want to add another keyboard,
+
+#include QMK_KEYBOARD_H
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+
+enum ferris_layers {
+  _BASE,
+  _NUM,
+  _SYMB,
+  _MOUSE,
+  _ARROWS,
+  _FUNCTIONS
+};
+
+enum tap_dance_codes {
+  DANCE_0,
+  DANCE_1
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_BASE] = LAYOUT(
+    KC_MINUS, KC_W, LT(_FUNCTIONS,KC_F), KC_P, KC_B,                      KC_Q, KC_U, KC_Y, KC_Z, KC_QUOTE,
+    MT(MOD_LALT | MOD_LCTL | MOD_LGUI, KC_A), MT(MOD_LALT, KC_R), LT(_ARROWS,KC_S), MT(MOD_LSFT, KC_T),KC_G,      KC_M, MT(MOD_RSFT, KC_N), KC_E, MT(MOD_LALT, KC_I), MT(MOD_RCTL, KC_O),
+  MT(MOD_LCTL, KC_ESCAPE), KC_X, KC_C, LT(_MOUSE,KC_D), KC_V,         KC_DOT, KC_H, KC_J, KC_K, KC_L,
+        LT(_NUM,KC_SPACE), LT(_SYMB,KC_TAB),      MT(MOD_RSFT, KC_ENTER),KC_BSPC
+  ),
+
+  [_NUM] = LAYOUT(
+    KC_TRNS, KC_SLASH, KC_BSLS, KC_PIPE, KC_NO,              KC_UNDS,  KC_1, KC_2, KC_3, KC_TRNS,
+    KC_LABK, KC_LPRN, KC_RPRN,  KC_RABK, KC_NO,              KC_MINUS, KC_4, KC_5, KC_6, KC_PLUS,
+    KC_LCBR, KC_LBRC,  KC_RBRC, KC_RCBR, KC_NO,              KC_TRNS,  KC_7, KC_8, KC_9, KC_0,
+                                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  ),
+
+  [_SYMB] = LAYOUT(
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                       KC_NO, KC_EXLM, KC_AT,    KC_HASH, KC_TRNS,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                       KC_NO, KC_DLR,  KC_PERC,  KC_CIRC, KC_EQUAL,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                       KC_TRNS, KC_AMPR,        KC_ASTR,        KC_LPRN,        KC_RPRN,
+                                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  ),
+  [_MOUSE] = LAYOUT(
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                       KC_NO, KC_NO, KC_MS_BTN3, KC_MS_BTN3, KC_TRNS,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                       KC_NO, KC_MS_BTN1, KC_MS_WH_DOWN, KC_MS_WH_UP, KC_MS_BTN2,
+    KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO,                       KC_TRNS, KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP, KC_MS_RIGHT,
+                                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  ),
+  [_ARROWS] = LAYOUT(
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                       KC_NO, KC_QUES, KC_GRAVE, KC_TILD,  KC_NO,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO,                       KC_NO, KC_COLN, KC_SCLN,  KC_QUOTE, KC_DQUO,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                       KC_TRNS, KC_LEFT,  KC_DOWN, KC_UP, KC_RIGHT,
+                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  ),
+  [_FUNCTIONS] = LAYOUT(
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_NO, TD(DANCE_0), TD(DANCE_1), KC_F3, KC_TRANSPARENT,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO,                KC_NO, KC_F4,       KC_F5,       KC_F6, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_NO, KC_F7,       KC_F8,       KC_F9, KC_F10,
+                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  )
+};
+
+const uint16_t PROGMEM esc_1[] = { MT(MOD_RSFT, KC_N), KC_E, COMBO_END };
+const uint16_t PROGMEM esc_2[] = { KC_E, MT(MOD_LALT, KC_I), COMBO_END };
+
+combo_t key_combos[] = {
+    COMBO(esc_1, KC_ESCAPE),
+    COMBO(esc_2, KC_ESCAPE),
+};
+
+typedef struct {
+    bool is_press_action;
+    uint8_t step;
+} tap;
+
+enum {
+    SINGLE_TAP = 1,
+    SINGLE_HOLD,
+    DOUBLE_TAP,
+    DOUBLE_HOLD,
+    DOUBLE_SINGLE_TAP,
+    MORE_TAPS
+};
+
+static tap dance_state[2];
+
+uint8_t dance_step(tap_dance_state_t *state);
+
+uint8_t dance_step(tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (state->interrupted || !state->pressed) return SINGLE_TAP;
+        else return SINGLE_HOLD;
+    } else if (state->count == 2) {
+        if (state->interrupted) return DOUBLE_SINGLE_TAP;
+        else if (state->pressed) return DOUBLE_HOLD;
+        else return DOUBLE_TAP;
+    }
+    return MORE_TAPS;
+}
+
+
+void on_dance_0(tap_dance_state_t *state, void *user_data);
+void dance_0_finished(tap_dance_state_t *state, void *user_data);
+void dance_0_reset(tap_dance_state_t *state, void *user_data);
+
+void on_dance_0(tap_dance_state_t *state, void *user_data) {
+    if(state->count == 3) {
+        tap_code16(KC_F1);
+        tap_code16(KC_F1);
+        tap_code16(KC_F1);
+    }
+    if(state->count > 3) {
+        tap_code16(KC_F1);
+    }
+}
+
+void dance_0_finished(tap_dance_state_t *state, void *user_data) {
+    dance_state[0].step = dance_step(state);
+    switch (dance_state[0].step) {
+        case SINGLE_TAP: register_code16(KC_F1); break;
+        case DOUBLE_TAP: register_code16(KC_F11); break;
+        case DOUBLE_SINGLE_TAP: tap_code16(KC_F1); register_code16(KC_F1);
+    }
+}
+
+void dance_0_reset(tap_dance_state_t *state, void *user_data) {
+    wait_ms(10);
+    switch (dance_state[0].step) {
+        case SINGLE_TAP: unregister_code16(KC_F1); break;
+        case DOUBLE_TAP: unregister_code16(KC_F11); break;
+        case DOUBLE_SINGLE_TAP: unregister_code16(KC_F1); break;
+    }
+    dance_state[0].step = 0;
+}
+void on_dance_1(tap_dance_state_t *state, void *user_data);
+void dance_1_finished(tap_dance_state_t *state, void *user_data);
+void dance_1_reset(tap_dance_state_t *state, void *user_data);
+
+void on_dance_1(tap_dance_state_t *state, void *user_data) {
+    if(state->count == 3) {
+        tap_code16(KC_F2);
+        tap_code16(KC_F2);
+        tap_code16(KC_F2);
+    }
+    if(state->count > 3) {
+        tap_code16(KC_F2);
+    }
+}
+
+void dance_1_finished(tap_dance_state_t *state, void *user_data) {
+    dance_state[1].step = dance_step(state);
+    switch (dance_state[1].step) {
+        case SINGLE_TAP: register_code16(KC_F2); break;
+        case DOUBLE_TAP: register_code16(KC_F12); break;
+        case DOUBLE_SINGLE_TAP: tap_code16(KC_F2); register_code16(KC_F2);
+    }
+}
+
+void dance_1_reset(tap_dance_state_t *state, void *user_data) {
+    wait_ms(10);
+    switch (dance_state[1].step) {
+        case SINGLE_TAP: unregister_code16(KC_F2); break;
+        case DOUBLE_TAP: unregister_code16(KC_F12); break;
+        case DOUBLE_SINGLE_TAP: unregister_code16(KC_F2); break;
+    }
+    dance_state[1].step = 0;
+}
+
+tap_dance_action_t tap_dance_actions[] = {
+        [DANCE_0] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_0, dance_0_finished, dance_0_reset),
+        [DANCE_1] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_1, dance_1_finished, dance_1_reset),
+};
+
+const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
+const key_override_t dot_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, KC_COMMA);
+
+// This globally defines all key overrides to be used
+const key_override_t **key_overrides = (const key_override_t *[]){
+	&delete_key_override,
+  &dot_key_override,
+	NULL // Null terminate the array of overrides!
+};
